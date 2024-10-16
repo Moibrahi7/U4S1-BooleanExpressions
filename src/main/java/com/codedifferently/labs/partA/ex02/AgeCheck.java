b569c64a9124dbaf598c2337ec9d1a721078daca
Moibrahi7 <moibrahi7@gmail.com>
2024-10-16 14:27:18 -0400

    Feat: Did more stuff

--- src/main/java/com/codedifferently/labs/partA/ex02/AgeCheck.java
+++ src/main/java/com/codedifferently/labs/partA/ex02/AgeCheck.java
@@ -3,7 +3,7 @@ package com.codedifferently.labs.partA.ex02;
 public class AgeCheck {
     public static String age(){
         String response= "";
- 
+
         int myAge = 19;   // I am 19; let me see if I can drive yet
 
         if (myAge >= 16) {
