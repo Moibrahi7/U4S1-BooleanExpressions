9a2f6013eb6c270dafbfc6a076d4a0d0a085af26
Moibrahi7 <moibrahi7@gmail.com>
2024-10-16 15:11:36 -0400

    Feat: did stuff some more some more t

--- src/test/java/com/codedifferently/labs/partA/ex02/AgeCheckTest.java
+++ src/test/java/com/codedifferently/labs/partA/ex02/AgeCheckTest.java
@@ -4,7 +4,7 @@ import com.codedifferently.labs.partA.ex02.AgeCheck;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class AgeCheckTest {
+public class AgeCheckTest { 
     @Test
     public void ageTest01(){
         String expected = "I'm not old enough yet... :*(";
